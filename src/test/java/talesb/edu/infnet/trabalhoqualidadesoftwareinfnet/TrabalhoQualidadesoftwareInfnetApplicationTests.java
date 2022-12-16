f29a5d868674baf279c98fe09a30a4119326709e
talesb <tales.batista@temasistemas.com.br>
2022-12-15 23:43:07 -0300

    Primeiro commit

---
+++ src/test/java/talesb/edu/infnet/trabalhoqualidadesoftwareinfnet/TrabalhoQualidadesoftwareInfnetApplicationTests.java
@@ -0,0 +1,13 @@
+package talesb.edu.infnet.trabalhoqualidadesoftwareinfnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoQualidadesoftwareInfnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
