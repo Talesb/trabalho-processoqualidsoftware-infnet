538aabcd706980db25db00c672553afe61ec9b16
talesb <tales.batista@temasistemas.com.br>
2023-03-24 21:48:12 -0300

    Ajustando service

--- src/main/java/talesb/edu/infnet/qualidadesoftware/service/TicketService.java
+++ src/main/java/talesb/edu/infnet/qualidadesoftware/service/TicketService.java
@@ -18,7 +18,6 @@ public class TicketService {
 
 		System.out.println("Ticket Owner: " + ticket.getPerson().getName().getFirstName());
 		System.out.println("Ticket Agent: " + ticket.getAssignedAgent().getName().getFirstName());
-		System.out.println("Commit example");
 
 		ticket.getProduct().forEach(product -> {
 			System.out.println("Ticket Product: "+product.getName());
